5b9c4e2267c8b4e2bb8b7a26df3ea7d378710fd1
saguirr <41924332+saguirr@users.noreply.github.com>
2021-02-10 10:36:58 -0500

    commit

--- js/app.js
+++ js/app.js
@@ -8,9 +8,9 @@ const resultado = document.querySelector("#resultado");
 const contenedorSimulacion = document.querySelector('#simulacion');
 
 //Variable tasa
-const tasa180 = 0.045;
-const tasa270 = 0.05;
-const tasa360 = 0.055;
+const tasa180 = 0.0275;
+const tasa270 = 0.03;
+const tasa360 = 0.035;
 
 let tasa;
 
